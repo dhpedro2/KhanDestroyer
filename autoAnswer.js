f3c357db06a3aa5872184bf25ceb371ce6978379
iUnknown                    ث <147574769+iUnknownBr@users.noreply.github.com>
2025-03-04 23:30:13 -0300

    Update autoAnswer.js

--- autoAnswer.js
+++ autoAnswer.js
@@ -1,4 +1,4 @@
-const baseClasses = ["_19uopuu", "_ssxvf9l", "_4i5p5ae", "_1r8cd7xe", "_1yok8f4"];
+const baseClasses = ["_s6zfc1u", "_ssxvf9l", "_4i5p5ae", "_1r8cd7xe", "_1yok8f4"];
 khanwareDominates = true;
 
 (async () => { 
